131864780bfb11874a9844ae324bed65ff57d4e3
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-05-03 14:28:24 -0300

    feat: create FeedbackSuccessStep

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,32 @@
-export function FeedbackSuccessStep() {
-  return <h1>FeedBack Success Step</h1>;
+import { CloseButton } from "../CloseButton";
+
+import successImageUrl from "../../../assets/success.svg";
+
+type FeedbackSuccessStepProps = {
+  onFeedbackRestartRequested: () => void;
+};
+
+export function FeedbackSuccessStep({
+  onFeedbackRestartRequested,
+}: FeedbackSuccessStepProps) {
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src={successImageUrl} alt="Success" />
+
+        <span className="text-xl mt-2">Agradecemos o feedback!</span>
+
+        <button
+          onClick={() => onFeedbackRestartRequested()}
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+        >
+          Quero enviar outro
+        </button>
+      </div>
+    </>
+  );
 }
--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -5,6 +5,7 @@ import ideaImageUrl from "../../assets/idea.svg";
 import otherImageUrl from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export type FeedbackType = keyof typeof feedbackOptions;
 
@@ -34,20 +35,31 @@ export const feedbackOptions = {
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
     setFeedbackType(null);
+    setFeedbackSent(false);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeSelect={setFeedbackType} />
-      ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackType}
+      {feedbackSent ? (
+        <FeedbackSuccessStep
           onFeedbackRestartRequested={handleRestartFeedback}
         />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeSelect={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-zinc-400">
