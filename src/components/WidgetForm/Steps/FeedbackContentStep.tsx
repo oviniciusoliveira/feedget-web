ae2889cb45fdf68ad967d6698ae25afa7f00aa2a
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-05-03 14:12:09 -0300

    feat: disable send feedback button if there is no comment

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -67,8 +67,9 @@ export function FeedbackContentStep({
           />
 
           <button
+            disabled={comment.length === 0}
             type="submit"
-            className="flex-1 p-2 bg-brand-500 rounded-md border-transparent justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="flex-1 p-2 bg-brand-500 rounded-md border-transparent justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
             Enviar feedback
           </button>
